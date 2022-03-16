d5a3571b3b229d9fe0cfdb5ee46cca230c8aed27
Cekiso <nonkululeko@quirky30.co.za>
2022-03-16 16:02:37 +0200

    update

--- word.dom.js
+++ word.dom.js
@@ -2,6 +2,7 @@ const button = document.querySelector('.submit');
 const message = document.querySelector('.message');
 const sentence = document.querySelector('.sentence');
 const words = document.querySelector('.counter');
+const checkbox = document.querySelector('.myCheckbox')
 
 
 
@@ -13,28 +14,45 @@ const wordBttnclicked = () => {
     for (var i = 0; i < text.length; i++) {
         const wordelement = text[i];
 
-
-
-
         if (wordelement.length > 4) {
-            arr += `<mark>${wordelement} </mark>`
-
+            arr += `<mark>${wordelement}</mark> `
         } else {
             arr += wordelement + " ";
-
-
         }
         if (wordelement.length > 0) {
             counter++
         }
 
-
         // arr.push(word)
-
         message.innerHTML = arr;
-
-
     }
     words.innerHTML = counter;
+
+
 }
+
+function checkHide() {
+    const word = sentence.value;
+    const split = word.split(" ");
+    let arr1 = "";
+
+    for (var i = 0; i < split.length; i++) {
+        const wordelement = split[i];
+
+        if ((checkbox.checked == true) && (split[i].length >= 5)) {
+            arr1 += `<mark>${wordelement}</mark> `
+        } else {
+
+            arr1 += ''
+        }
+        if (split[i].length >= 8) {
+            arr1 += `<mark>${wordelement}</mark> `
+        }
+        message.innerHTML = arr1
+    }
+
+}
+
+
+checkbox.addEventListener('change', checkHide)
 button.addEventListener('click', wordBttnclicked)
