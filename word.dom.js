452ba43969589b1bfd5cb760f343752358c2f4d0
Cekiso <nonkululeko@quirky30.co.za>
2022-03-16 10:58:24 +0200

    update

--- word.dom.js
+++ word.dom.js
@@ -1,26 +1,40 @@
 const button = document.querySelector('.submit');
 const message = document.querySelector('.message');
-const sentence = document.querySelector('.sentence')
+const sentence = document.querySelector('.sentence');
+const words = document.querySelector('.counter');
 
 
 
 const wordBttnclicked = () => {
+    let counter = 0;
     const word = sentence.value;
     const text = word.split(" ");
     let arr = "";
     for (var i = 0; i < text.length; i++) {
         const wordelement = text[i];
 
-        if (wordelement.split("").length > 4) {
-            arr += `<mark>${wordelement} </mark> `
+
+
+
+        if (wordelement.length > 4) {
+            arr += `<mark>${wordelement} </mark>`
+
         } else {
             arr += wordelement + " ";
+
+
         }
+        if (wordelement.length > 0) {
+            counter++
+        }
+
 
         // arr.push(word)
 
         message.innerHTML = arr;
-        console.log(word);
+
+
     }
+    words.innerHTML = counter;
 }
 button.addEventListener('click', wordBttnclicked)
